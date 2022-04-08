ec6734f566a32a16bc390dcc60847d09d9dce2f2
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-04-08 15:55:31 -0400

    rewrite the server's 'changed' callback

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,51 @@
 import path from 'node:path';
+import { isPromise } from 'node:util/types';
 import { watch as filewatch } from 'chokidar';
 
 import { createDebug } from '../utils/log.js';
 import Server from './http.js';
 import Reloader from './reloader.js';
 
+const debug = createDebug('watcher');
+
 /**
  *
  * @param {object} config
  * @param {string} config.dir
  * @param {number} [config.port=3000]
- * @param {({ path: string, file: string }) => void} [changed]
+ * @param {(path: string) => void | Promise<void>} [changed]
+ *
  * @returns {Promise<void>}
  */
-async function serve(config, changed) {
+async function serve(
+	config,
+	/**
+	 *
+	 * A function to run each time there's a file changed.
+	 * Most likely, you'll want some sort of build function
+	 * here.
+	 */
+	changed,
+) {
 	const s = new Server({ dir: config.dir });
 	const t = new Reloader();
 
 	await s.listen(config.port);
 	await t.listen();
 
-	const debug = createDebug('watcher');
-
 	const watcher = filewatch(config.dir);
 	debug('running');
 
-	watcher.on('change', (fpath, _fstats) => {
-		const p = '/' + path.relative(config.dir, fpath);
-		if (changed) changed({ path: p, file: fpath });
-		t.reload(p);
+	watcher.on('change', async (fpath, _fstats) => {
+		const relativePath = path.relative(config.dir, fpath);
+		debug('Changed - %s', relativePath);
+
+		if (changed) {
+			const value = changed(relativePath);
+			if (isPromise(value)) await value;
+		}
+
+		t.reload('/' + relativePath);
 	});
 }
 
